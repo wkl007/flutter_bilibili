4f833e389f14786391a86ad35a18df993b91800f
wkl007 <499657357@qq.com>
2021-06-20 09:41:41 +0800

    feat: 上拉加载更多与分页功能实现

--- lib/pages/home_tab_page.dart
+++ lib/pages/home_tab_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bilibili/http/core/hi_error.dart';
 import 'package:flutter_bilibili/http/dao/home_dao.dart';
 import 'package:flutter_bilibili/model/home_model.dart';
+import 'package:flutter_bilibili/util/color.dart';
 import 'package:flutter_bilibili/util/toast.dart';
 import 'package:flutter_bilibili/widgets/hi_banner.dart';
 import 'package:flutter_bilibili/widgets/video_card.dart';
@@ -29,6 +30,12 @@ class _HomeTabPageState extends State<HomeTabPage>
   /// 第一页
   int pageIndex = 1;
 
+  /// 加载状态
+  bool loading = false;
+
+  /// 控制器
+  ScrollController _scrollController = ScrollController();
+
   /// 页面缓存
   @override
   bool get wantKeepAlive => true;
@@ -36,16 +43,34 @@ class _HomeTabPageState extends State<HomeTabPage>
   @override
   void initState() {
     super.initState();
+    _scrollController.addListener(() {
+      var dis = _scrollController.position.maxScrollExtent -
+          _scrollController.position.pixels;
+      if (dis < 300 &&
+          !loading &&
+          // fix 当列表高度不满屏幕高度时不执行加载更多
+          _scrollController.position.maxScrollExtent != 0) {
+        _loadData(loadMore: true);
+      }
+    });
     _loadData();
   }
 
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   /// 加载数据
-  _loadData({loadMore: false}) async {
+  Future<void> _loadData({loadMore: false}) async {
     try {
+      if (loading) return;
+      loading = true;
       if (!loadMore) pageIndex = 1;
       int currentIndex = pageIndex + (loadMore ? 1 : 0);
       HomeModel res = await HomeDao.get(widget.categoryName,
-          pageIndex: currentIndex, pageSize: 50);
+          pageIndex: currentIndex);
       setState(() {
         if (loadMore) {
           videoList = [...videoList, ...res.videoList];
@@ -56,9 +81,14 @@ class _HomeTabPageState extends State<HomeTabPage>
           videoList = res.videoList;
         }
       });
+      Future.delayed(Duration(milliseconds: 1000), () {
+        loading = false;
+      });
     } on NeedAuth catch (e) {
+      loading = false;
       showWarnToast(e.message);
     } on HiNetError catch (e) {
+      loading = false;
       showWarnToast(e.message);
     }
   }
@@ -73,24 +103,29 @@ class _HomeTabPageState extends State<HomeTabPage>
 
   @override
   Widget build(BuildContext context) {
-    return MediaQuery.removePadding(
-      removeTop: true,
-      context: context,
-      child: HiNestedScrollView(
-        itemCount: videoList.length,
-        padding: EdgeInsets.only(top: 10, left: 10, right: 10),
-        headers: [
-          if (widget.bannerList != null)
-            Padding(
-              padding: EdgeInsets.only(bottom: 8),
-              child: _banner(widget.bannerList!),
-            ),
-        ],
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2, childAspectRatio: 0.95),
-        itemBuilder: (BuildContext context, int index) {
-          return VideoCard(videoInfo: videoList[index]);
-        },
+    return RefreshIndicator(
+      onRefresh: _loadData,
+      color: primary,
+      child: MediaQuery.removePadding(
+        removeTop: true,
+        context: context,
+        child: HiNestedScrollView(
+          controller: _scrollController,
+          itemCount: videoList.length,
+          padding: EdgeInsets.only(top: 10, left: 10, right: 10),
+          headers: [
+            if (widget.bannerList != null)
+              Padding(
+                padding: EdgeInsets.only(bottom: 8),
+                child: _banner(widget.bannerList!),
+              ),
+          ],
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, childAspectRatio: 0.95),
+          itemBuilder: (BuildContext context, int index) {
+            return VideoCard(videoInfo: videoList[index]);
+          },
+        ),
       ),
     );
   }
