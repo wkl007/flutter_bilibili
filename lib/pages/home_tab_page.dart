40af4f38ed372c11e5bac2d93d40827dc8346999
wkl007 <499657357@qq.com>
2021-06-20 14:17:01 +0800

    feat: HiBaseTabState 组件封装

--- lib/pages/home_tab_page.dart
+++ lib/pages/home_tab_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter_bilibili/model/home_model.dart';
 import 'package:flutter_bilibili/util/color.dart';
 import 'package:flutter_bilibili/util/toast.dart';
 import 'package:flutter_bilibili/widgets/hi_banner.dart';
+import 'package:flutter_bilibili/widgets/hi_base_tab_state.dart';
 import 'package:flutter_bilibili/widgets/video_card.dart';
 import 'package:flutter_nested/flutter_nested.dart';
 
@@ -22,77 +23,8 @@ class HomeTabPage extends StatefulWidget {
   _HomeTabPageState createState() => _HomeTabPageState();
 }
 
-class _HomeTabPageState extends State<HomeTabPage>
-    with AutomaticKeepAliveClientMixin {
-  /// 视频列表
-  List<VideoModel> videoList = [];
-
-  /// 第一页
-  int pageIndex = 1;
-
-  /// 加载状态
-  bool loading = false;
-
-  /// 控制器
-  ScrollController _scrollController = ScrollController();
-
-  /// 页面缓存
-  @override
-  bool get wantKeepAlive => true;
-
-  @override
-  void initState() {
-    super.initState();
-    _scrollController.addListener(() {
-      var dis = _scrollController.position.maxScrollExtent -
-          _scrollController.position.pixels;
-      if (dis < 300 &&
-          !loading &&
-          // fix 当列表高度不满屏幕高度时不执行加载更多
-          _scrollController.position.maxScrollExtent != 0) {
-        _loadData(loadMore: true);
-      }
-    });
-    _loadData();
-  }
-
-  @override
-  void dispose() {
-    _scrollController.dispose();
-    super.dispose();
-  }
-
-  /// 加载数据
-  Future<void> _loadData({loadMore: false}) async {
-    try {
-      if (loading) return;
-      loading = true;
-      if (!loadMore) pageIndex = 1;
-      int currentIndex = pageIndex + (loadMore ? 1 : 0);
-      HomeModel res = await HomeDao.get(widget.categoryName,
-          pageIndex: currentIndex);
-      setState(() {
-        if (loadMore) {
-          videoList = [...videoList, ...res.videoList];
-          if (res.videoList.length != 0) {
-            pageIndex++;
-          }
-        } else {
-          videoList = res.videoList;
-        }
-      });
-      Future.delayed(Duration(milliseconds: 1000), () {
-        loading = false;
-      });
-    } on NeedAuth catch (e) {
-      loading = false;
-      showWarnToast(e.message);
-    } on HiNetError catch (e) {
-      loading = false;
-      showWarnToast(e.message);
-    }
-  }
-
+class _HomeTabPageState
+    extends HiBaseTabState<HomeModel, VideoModel, HomeTabPage> {
   /// 轮播图
   _banner(List<BannerModel> bannerList) {
     return HiBanner(
@@ -102,16 +34,47 @@ class _HomeTabPageState extends State<HomeTabPage>
   }
 
   @override
+  get contentChild => HiNestedScrollView(
+        controller: scrollController,
+        itemCount: dataList.length,
+        padding: EdgeInsets.only(top: 10, left: 10, right: 10),
+        headers: [
+          if (widget.bannerList != null)
+            Padding(
+              padding: EdgeInsets.only(bottom: 8),
+              child: _banner(widget.bannerList!),
+            ),
+        ],
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, childAspectRatio: 0.95),
+        itemBuilder: (BuildContext context, int index) {
+          return VideoCard(videoInfo: dataList[index]);
+        },
+      );
+
+  @override
+  Future<HomeModel> getData(int pageIndex) async {
+    HomeModel result =
+        await HomeDao.get(widget.categoryName, pageIndex: pageIndex);
+    return result;
+  }
+
+  @override
+  List<VideoModel> parseList(HomeModel result) {
+    return result.videoList;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return RefreshIndicator(
-      onRefresh: _loadData,
+      onRefresh: loadData,
       color: primary,
       child: MediaQuery.removePadding(
         removeTop: true,
         context: context,
         child: HiNestedScrollView(
-          controller: _scrollController,
-          itemCount: videoList.length,
+          controller: scrollController,
+          itemCount: dataList.length,
           padding: EdgeInsets.only(top: 10, left: 10, right: 10),
           headers: [
             if (widget.bannerList != null)
@@ -123,7 +86,7 @@ class _HomeTabPageState extends State<HomeTabPage>
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2, childAspectRatio: 0.95),
           itemBuilder: (BuildContext context, int index) {
-            return VideoCard(videoInfo: videoList[index]);
+            return VideoCard(videoInfo: dataList[index]);
           },
         ),
       ),
---
+++ lib/widgets/hi_base_tab_state.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bilibili/http/core/hi_error.dart';
+import 'package:flutter_bilibili/util/color.dart';
+import 'package:flutter_bilibili/util/hi_state.dart';
+import 'package:flutter_bilibili/util/toast.dart';
+
+/// 通用底层带分页和刷新的页面框架
+/// M为Dao返回数据模型，L为列表数据模型，T为具体widget
+abstract class HiBaseTabState<M, L, T extends StatefulWidget> extends HiState<T>
+    with AutomaticKeepAliveClientMixin {
+  /// 列表
+  List<L> dataList = [];
+
+  /// 第一页
+  int pageIndex = 1;
+
+  /// 加载状态
+  bool loading = false;
+
+  /// 控制器
+  ScrollController scrollController = ScrollController();
+
+  get contentChild;
+
+  /// 页面缓存
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController.addListener(() {
+      var dis = scrollController.position.maxScrollExtent -
+          scrollController.position.pixels;
+      if (dis < 300 &&
+          !loading &&
+          // fix 当列表高度不满屏幕高度时不执行加载更多
+          scrollController.position.maxScrollExtent != 0) {
+        loadData(loadMore: true);
+      }
+    });
+    loadData();
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  ///获取对应页码的数据
+  Future<M> getData(int pageIndex);
+
+  ///从MO中解析出list数据
+  List<L> parseList(M result);
+
+  /// 加载数据
+  Future<void> loadData({loadMore = false}) async {
+    if (loading) return;
+    loading = true;
+    if (!loadMore) pageIndex = 1;
+    var currentIndex = pageIndex + (loadMore ? 1 : 0);
+    try {
+      var result = await getData(currentIndex);
+      setState(() {
+        if (loadMore) {
+          // 合成一个新数组
+          dataList = [...dataList, ...parseList(result)];
+          if (parseList(result).length != 0) {
+            pageIndex++;
+          }
+        } else {
+          dataList = parseList(result);
+        }
+      });
+      Future.delayed(Duration(milliseconds: 1000), () {
+        loading = false;
+      });
+    } on NeedAuth catch (e) {
+      loading = false;
+      showWarnToast(e.message);
+    } on HiNetError catch (e) {
+      loading = false;
+      showWarnToast(e.message);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return RefreshIndicator(
+      onRefresh: loadData,
+      color: primary,
+      child: MediaQuery.removePadding(
+        removeTop: true,
+        context: context,
+        child: contentChild,
+      ),
+    );
+  }
+}
