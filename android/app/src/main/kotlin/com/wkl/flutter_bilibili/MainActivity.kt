71cb1a1784e2429abe7703797d36f7275ecb5214
wkl007 <499657357@qq.com>
2021-06-20 19:08:36 +0800

    feat: 友盟统计集成，打包混淆

--- android/app/src/main/kotlin/com/wkl/flutter_bilibili/MainActivity.kt
+++ android/app/src/main/kotlin/com/wkl/flutter_bilibili/MainActivity.kt
@@ -1,6 +1,24 @@
 package com.wkl.flutter_bilibili
 
-import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
 
-class MainActivity: FlutterActivity() {
+import io.flutter.embedding.android.FlutterActivity
+import com.umeng.analytics.MobclickAgent
+import com.umeng.commonsdk.UMConfigure
+
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        UMConfigure.init(this, "60cf1e5626a57f10182e89ba", "Flutter BiliBili", UMConfigure.DEVICE_TYPE_PHONE, null)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        MobclickAgent.onPause(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        MobclickAgent.onResume(this)
+    }
 }
