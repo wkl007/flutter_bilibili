c20e162e45f7e098874ecf475d5324fe005e4817
wkl007 <499657357@qq.com>
2021-06-20 14:29:22 +0800

    feat: 排行榜接口封装

---
+++ lib/http/dao/ranking_dao.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_bilibili/http/core/hi_net.dart';
+import 'package:flutter_bilibili/http/request/ranking_request.dart';
+import 'package:flutter_bilibili/model/ranking_model.dart';
+
+class RankingDao {
+  //https://api.devio.org/uapi/fa/ranking?sort=like&pageIndex=1&pageSize=40
+  static get(String sort, {int pageIndex = 1, pageSize = 10}) async {
+    RankingRequest request = RankingRequest();
+    request
+        .add('sort', sort)
+        .add('pageIndex', pageIndex)
+        .add('pageSize', pageSize)
+    var result = await HiNet.getInstance().fire(request);
+    return RankingModel.fromJson(result['data']);
+  }
+}
---
+++ lib/model/ranking_model.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+import 'package:flutter_bilibili/util/model_util.dart';
+import 'package:flutter_bilibili/model/home_model.dart';
+
+class RankingModel {
+  RankingModel({
+    required this.total,
+    required this.list,
+  });
+
+  factory RankingModel.fromJson(Map<String, dynamic> jsonRes) {
+    final List<VideoModel>? list =
+        jsonRes['list'] is VideoModel ? <VideoModel>[] : null;
+    if (list != null) {
+      for (final dynamic item in jsonRes['list']!) {
+        if (item != null) {
+          list.add(VideoModel.fromJson(asT<Map<String, dynamic>>(item)!));
+        }
+      }
+    }
+    return RankingModel(
+      total: asT<int>(jsonRes['total'])!,
+      list: list!,
+    );
+  }
+
+  int total;
+  List<VideoModel> list;
+
+  @override
+  String toString() {
+    return jsonEncode(this);
+  }
+
+  Map<String, dynamic> toJson() => <String, dynamic>{
+        'total': total,
+        'list': list,
+      };
+
+  RankingModel clone() => RankingModel.fromJson(
+      asT<Map<String, dynamic>>(jsonDecode(jsonEncode(this)))!);
+}
